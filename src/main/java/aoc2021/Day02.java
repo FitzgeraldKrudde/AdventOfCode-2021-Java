14dc9b20006844b5a4164642924d60d1a147f1ab
Fitzgerald Krudde <playstore@krudde.nl>
2021-12-05 22:13:33 +0100

    day 2: nice and easy, and using a switch expression

---
+++ src/main/java/aoc2021/Day02.java
@@ -0,0 +1,77 @@
+package aoc2021;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class Day02 extends Day {
+    // @formatter:off
+    static public void main(String[] args) throws Exception {
+        // get our class
+        final Class<?> clazz = new Object() {}.getClass().getEnclosingClass();
+
+        // get our full classname
+        final String fullClassName = clazz.getCanonicalName();
+
+        // construct filename with input
+        final String filename = clazz.getSimpleName().toLowerCase().replace("_0", "_") + ".txt";
+
+        // create instance
+        Object day = Class.forName(fullClassName).getDeclaredConstructor().newInstance();
+        // find run method
+        Method runMethod = day.getClass().getMethod("main", String.class);
+        // invoke run method for our instance
+        runMethod.invoke(day, filename);
+
+        // @formatter:on
+    }
+
+    @Override
+    public String doPart1(List<String> inputRaw) {
+        long forward = countMoves(inputRaw, "forward");
+        long up = countMoves(inputRaw, "up");
+        long down = countMoves(inputRaw, "down");
+
+        return String.valueOf(forward * (down - up));
+    }
+
+    private long countMoves(List<String> inputRaw, String direction) {
+        return inputRaw.stream()
+                .filter(line -> line.startsWith(direction))
+                .map(line -> line.split("\\s+")[1])
+                .mapToLong(Long::valueOf)
+                .sum();
+    }
+
+    @Override
+    public String doPart2(List<String> inputRaw) {
+
+        Submarine submarine = new Submarine(new Position(0, 0, 0));
+        inputRaw.stream()
+                .forEach(submarine::move);
+
+        return String.valueOf(submarine.position.horizontal() * submarine.position.depth());
+    }
+
+    record Position(long horizontal, long depth, long aim) {
+    }
+
+    static class Submarine {
+        private Position position;
+
+        Submarine(Position position) {
+            this.position = position;
+        }
+
+        public void move(String instruction) {
+            String[] words = instruction.split("\\s+");
+            String direction = words[0];
+            long moves = Long.parseLong(words[1]);
+            position = switch (direction) {
+                case "up" -> new Position(position.horizontal, position.depth, position.aim - moves);
+                case "down" -> new Position(position.horizontal, position.depth, position.aim + moves);
+                case "forward" -> new Position(position.horizontal + moves, position.depth + (position.aim * moves), position.aim);
+                default -> throw new RuntimeException("unknown instruction");
+            };
+        }
+    }
+}
